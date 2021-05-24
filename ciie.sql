d1ed050bae8138b7d8c205b1601131063ba825ae
ffreddymx <ffreddy.mx@gmail.com>
2021-05-23 19:42:46 -0500

    agregar

--- ciie.sql
+++ ciie.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 16-03-2021 a las 00:42:08
+-- Tiempo de generación: 24-05-2021 a las 02:42:25
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.2.29
 
@@ -20,6 +20,53 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `ciie`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `emprendedor`
+--
+
+CREATE TABLE `emprendedor` (
+  `id` int(11) NOT NULL,
+  `Nombre` varchar(50) NOT NULL,
+  `Apellido` varchar(50) NOT NULL,
+  `Direccion` varchar(200) NOT NULL,
+  `Movil` varchar(10) NOT NULL,
+  `Mail` varchar(50) NOT NULL,
+  `Profesion` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `emprendedor`
+--
+
+INSERT INTO `emprendedor` (`id`, `Nombre`, `Apellido`, `Direccion`, `Movil`, `Mail`, `Profesion`) VALUES
+(1, 'Samuel', 'Silvan', 'asdf', 'asdf', 'asdf', 'asdf'),
+(2, 'Fausto', 'Gomez', 'asdfasd', '9999999999', 'asdfasdf', 'asdfasdf'),
+(3, 'Efrain', 'Narvaez', 'gsdfgsdf', '9999999999', 'sdfgsdf', 'gsdfgsdfg');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proyectos`
+--
+
+CREATE TABLE `proyectos` (
+  `id` int(11) NOT NULL,
+  `Nombre` varchar(500) NOT NULL,
+  `Fecha` date NOT NULL,
+  `Estatus` varchar(20) NOT NULL,
+  `idemprendedor` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `proyectos`
+--
+
+INSERT INTO `proyectos` (`id`, `Nombre`, `Fecha`, `Estatus`, `idemprendedor`) VALUES
+(1, 'sdfgsdfg', '0000-00-00', 'Aceptado', 1),
+(2, 'Desarrollo industria renovable', '2021-05-23', 'Aceptado', 2);
 
 -- --------------------------------------------------------
 
@@ -47,6 +94,18 @@ INSERT INTO `userxwz` (`Id`, `usuario`, `password`, `Tipo`, `Nombre`) VALUES
 --
 
 --
+-- Indices de la tabla `emprendedor`
+--
+ALTER TABLE `emprendedor`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `proyectos`
+--
+ALTER TABLE `proyectos`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indices de la tabla `userxwz`
 --
 ALTER TABLE `userxwz`
@@ -55,6 +114,18 @@ ALTER TABLE `userxwz`
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `emprendedor`
+--
+ALTER TABLE `emprendedor`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `proyectos`
+--
+ALTER TABLE `proyectos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `userxwz`
