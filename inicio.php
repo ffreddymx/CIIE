27dde7ac68d1cdf0149e740f32d5b448d6bd7996
ffreddymx <ffreddy.mx@gmail.com>
2021-11-19 12:10:02 -0600

    diseño2

--- inicio.php
+++ inicio.php
@@ -5,10 +5,10 @@ include 'header.php';
 
 ?>
 
-<div align="center" style="background: white"> <img src="img/logo1.png" height="50px">  <img src="img/logo3.jpg" height="50px"> <img src="img/logo2.jpg" height="50px"> <img src="img/logo4.jpg" height="50px"> </div>
+<div align="center" style="background: white"> <img src="img/logo1.png" height="200px">  <img src="img/logo3.jpg" height="200px"> <img src="img/logo2.jpg" height="200px"> <img src="img/logo4.jpg" height="200px"> </div>
 
 		 <div align="center" style="margin-top: ">
- <img src="img/fondo.jpg" alt="" width = "1200" >
+ <img src="img/fondo.jpg" alt="" width = "900" >
 
 
  <?php include 'footer.php'; ?>
